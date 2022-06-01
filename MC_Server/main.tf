b728fed712e0fdaa61373c509a5183271dfeb30e
Sean Boyer <seanABoyer@gmail.com>
2022-05-31 21:19:39 -0500

    Folder Update and SG Rules Update

--- MC_Server/main.tf
+++ MC_Server/main.tf
@@ -8,12 +8,12 @@ module "server"{
     ssh_username = var.ssh_username
     root_block_size = 32
 }
-# module "application"{
-#     source = "../modules/MinecraftAppConfig"
-#     security_group_id = module.server.security_group_id
-#     cidr_block = module.server.cidr_block
-#     depends_on = [module.server]
-# }
+module "application"{
+    source = "../modules/MinecraftAppConfig"
+    security_group_id = module.server.security_group_id
+    cidr_block = module.server.cidr_block
+    depends_on = [module.server]
+}
 
 module "application_registration" {
     source = "../modules/RegisterServer"
