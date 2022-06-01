81f33c804ab78ed980c51355dd9ab7bc4cc7ce9c
Sean Boyer <seanABoyer@gmail.com>
2022-05-31 20:20:52 -0500

    v1

---
+++ MC_Server_01/main.tf
@@ -0,0 +1,25 @@
+module "server"{
+    source = "../modules/DebianServer"
+
+    game_name = "mc"
+    region = var.region
+    availability_zone = var.availability_zone
+    instance_type = "t2.medium"
+    private_ssh_key = var.private_ssh_key
+    ssh_username = var.ssh_username
+    root_block_size = 32
+}
+module "application"{
+    source = "../modules/MinecraftAppConfig"
+    security_group_id = module.server.security_group_id
+    cidr_block = module.server.cidr_block
+}
+
+module "application_registration" {
+    source = "../modules/RegisterServer"
+    dnsPrefix = "mc"
+    dnsZone = var.dnsZone
+    publicIP = module.server.publicIP
+    tableName = var.tableName
+    lgsmCommand = "mcserver"
+}
