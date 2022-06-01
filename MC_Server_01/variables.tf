9a87c6bf70230c28eee80706c23ef40375c821e5
Sean Boyer <seanABoyer@gmail.com>
2022-05-31 20:35:20 -0500

    variables

---
+++ MC_Server_01/variables.tf
@@ -0,0 +1,19 @@
+variable "region"{
+    type = string
+}
+variable "availability_zone"{
+    type = string
+}
+variable "private_ssh_key"{
+    type = string
+}
+variable "ssh_username"{
+    type = string
+}
+variable "dnsZone"{
+    type = string    
+}
+variable "tableName"{
+    type = string
+}
+
