a550a00dd9059d8da5cf8f5f6016ce39e35de9f5
Sean Boyer <seanABoyer@gmail.com>
2022-10-18 19:14:34 -0700

    Testing Script

--- modules/GameInstallScripts/minecraft_sevtech.sh
+++ modules/GameInstallScripts/minecraft_sevtech.sh
@@ -17,42 +17,42 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 finishLog "Updating System"
 
-startLog "Installing Packages"
-sudo apt-get install curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat openjdk-17-jre lib32gcc-s1 lib32stdc++6 libsdl2-2.0-0:i386 -y
-finishLog "Installing Packages"
+# startLog "Installing Packages"
+# sudo apt-get install curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat openjdk-17-jre lib32gcc-s1 lib32stdc++6 libsdl2-2.0-0:i386 -y
+# finishLog "Installing Packages"
 
 startLog "Creating User and Changing User"
 sudo useradd mcserver -p $password -m
 sudo chown -R mcserver:mcserver /home/mcserver
 finishLog "Creating User and Changing User"
 
-startLog "Download linuxgsm.sh install server"
-sudo -H -u mcserver bash -c "cd ~ && wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh mcserver "
-sudo -H -u mcserver bash -c "cd ~ && yes | ./mcserver install"
-finishLog "Download linuxgsm.sh and install server"
+# startLog "Download linuxgsm.sh install server"
+# sudo -H -u mcserver bash -c "cd ~ && wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh mcserver "
+# sudo -H -u mcserver bash -c "cd ~ && yes | ./mcserver install"
+# finishLog "Download linuxgsm.sh and install server"
 
-startLog "Config server"
-#Reduce Max Players to 10 players.
-sudo -H -u mcserver bash -c "cd ~ && sed -i 's/max-players=20/max-players=10/g' ./serverfiles/server.properties"
-#Change How much Ram the JVM Runs on                                /home/mcserver/lgsm/config-lgsm/mcserver
-sudo -H -u mcserver bash -c "cd ~ && echo 'javaram="3072"' >> ./lgsm/config-lgsm/mcserver/common.cfg"
-finishLog "Config server"
+# startLog "Config server"
+# #Reduce Max Players to 10 players.
+# sudo -H -u mcserver bash -c "cd ~ && sed -i 's/max-players=20/max-players=10/g' ./serverfiles/server.properties"
+# #Change How much Ram the JVM Runs on                                /home/mcserver/lgsm/config-lgsm/mcserver
+# sudo -H -u mcserver bash -c "cd ~ && echo 'javaram="3072"' >> ./lgsm/config-lgsm/mcserver/common.cfg"
+# finishLog "Config server"
 
 # startLog "Start server"
 # sudo -H -u mcserver bash -c "cd ~ && ./mcserver start"
 # finishLog "Start server"
 
 startLog "Downloading SevTech"
-wget https://www.curseforge.com/minecraft/modpacks/sevtech-ages/download/3570046 -o sevtech-server.zip
-unzip sevtech-server.zip sevtech-server
+sudo -H -u mcserver bash -c "mkdir -p /home/mcserver/sevtech"
+sudo -H -u mcserver bash -c "wget https://mediafilez.forgecdn.net/files/3570/46/SevTech_Ages_Server_3.2.3.zip -O sevtech-server.zip"
 
 finishLog "Downloading SevTech"
 
 startLog "Installing SevTech"
-cd sevtech-server
-sh Install.sh
+sudo -H -u mcserver bash -c "unzip sevtech-server.zip"
+sudo -H -u mcserver bash -c "sh Install.sh"
 finishLog "Installing SevTech"
 
 startLog "Starting SevTech"
-sh ServerStart.sh
+sudo -H -u mcserver bash -c "sh ServerStart.sh"
 finishLog "Starting SevTech"
