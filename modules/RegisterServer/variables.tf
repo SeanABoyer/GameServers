7f05eddc4d24b5c61c36871f5d1c8b9d4c83f311
Sean Boyer <seanABoyer@gmail.com>
2022-10-18 18:02:06 -0700

    Removing Old tinkering

--- modules/RegisterServer/variables.tf
+++ modules/RegisterServer/variables.tf
@@ -7,13 +7,12 @@ variable "dnsZone" {
 variable "public_ip"{
     type = string
 }
-variable "tableName" {
-    type = string
-    default = ""
-}
-variable "lgsmCommand"{
-    type = string
-}
-variable "ec2_instance_id" {
-    type = string
-}
+# variable "tableName" {
+#     type = string
+# }
+# variable "lgsmCommand"{
+#     type = string
+# }
+# variable "ec2_instance_id" {
+#     type = string
+# }
