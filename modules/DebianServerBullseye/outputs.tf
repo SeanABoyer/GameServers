3664798c61bfe552580a12e7c19aef91b04f8ac7
Sean Boyer <seanABoyer@gmail.com>
2022-11-09 16:23:44 -0800

    update output properly

--- modules/DebianServerBullseye/outputs.tf
+++ modules/DebianServerBullseye/outputs.tf
@@ -5,7 +5,7 @@ output "cidr_block" {
     value = aws_vpc.vpc.cidr_block
 }
 output "security_group_id" {
-    value = aws_security_group.security_group.id
+    value = aws_security_group.ec2_sg.id
 }
 output "ec2_instance_id" {
     value = aws_instance.server.id
