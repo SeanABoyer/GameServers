d032e85e36b4c4be256df7af6febd5f316275aea
Sean Boyer <seanABoyer@gmail.com>
2022-11-20 02:13:08 -0800

    edit fstab to remount EFS on restart

--- modules/GameInstallScripts/minecraft_sevtechskyblock.sh
+++ modules/GameInstallScripts/minecraft_sevtechskyblock.sh
@@ -54,6 +54,7 @@ startLog "Mounting EFS"
 mkdir -p $root_dir
 generalLog "Attempting to mount $filesystem_id to $root_dir"
 mount -t efs -o tls,iam "$filesystem_id" $root_dir
+echo "$filesystem_id:/ $root_dir efs _netdev,noresvport,tls,iam 0 0" >> /etc/fstab
 finishLog "Mounting EFS"
 
 startLog "Creating User and Changing User"
