bfd8e4bd27cfe12b06c07056cfb68cdefea1c4d3
Sean Boyer <seanABoyer@gmail.com>
2023-08-05 11:31:39 -0700

    Adding Script & Plan for Regrowth.

---
+++ plans/Regrowth_Server/variables.tf
@@ -0,0 +1,13 @@
+variable "public_ssh_key" {
+    type = string
+}
+variable "ssh_username"{
+    type = string
+}
+variable "dnsZone"{
+    type = string    
+}
+
+variable "dnsPrefix"{
+    type = string
+}
