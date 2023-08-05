c09b4ae02773a12d19460077a7c7d68bc8a43d81
Sean Boyer <seanABoyer@gmail.com>
2023-08-05 14:39:19 -0700

    Moving around how path variable works

--- modules/GameInstallScripts/minecraft_regrowth.sh
+++ modules/GameInstallScripts/minecraft_regrowth.sh
@@ -37,15 +37,15 @@ function startService(){
     systemctl start mcserver
     finishLog "Start Service"
 
-    #EULA file is created after service is started
-    # startLog "Accept EULA"
-    # while [ ! -f "$root_dir/eula.txt" ]
-    # do
-    #     generalLog "Unable to find [$root_dir/eula.txt]. Will check again in 10 seconds. "
-    #     sleep 10
-    # done
-    # sudo -H -u mcserver bash -c "cd $root_dir && sed -i 's/eula=false/eula=true/g' $root_dir/eula.txt"
-    # finishLog "Accept EULA"
+    EULA file is created after service is started
+    startLog "Accept EULA"
+    while [ ! -f "$root_dir/eula.txt" ]
+    do
+        generalLog "Unable to find [$root_dir/eula.txt]. Will check again in 10 seconds. "
+        sleep 10
+    done
+    sudo -H -u mcserver bash -c "cd $root_dir && sed -i 's/eula=false/eula=true/g' $root_dir/eula.txt"
+    finishLog "Accept EULA"
 }
 
 
@@ -77,7 +77,8 @@ chmod +x /home/mcserver/CloudWatchMetricGeneration.sh
 (crontab -l; echo "*/15 * * * * /home/mcserver/CloudWatchMetricGeneration.sh") | sort -u | crontab -
 finishLog "Setting up CronJob for Custom CloudWatch Metric"
 
-#If the ServerStart.sh does not exist, then download modpack and java to EFS share
+$start_file="$root_dir/MCServerStart.sh"
+#If the ServerStart.sh does not exist, then download modpack and java to EFS share and setup for future use
 if [ ! -f "$root_dir/ServerStart.sh" ]
 then
     generalLog "Unable to find [$root_dir/ServerStart.sh]."
@@ -85,10 +86,14 @@ then
     sudo -H -u mcserver bash -c "cd $root_dir && mkdir java"
     sudo -H -u mcserver bash -c "cd $root_dir/java && wget https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u345-b01/OpenJDK8U-jre_x64_linux_hotspot_8u345b01.tar.gz -O OpenJDK8.tar.gz"
     sudo -H -u mcserver bash -c "cd $root_dir/java && tar xf OpenJDK8.tar.gz"
-    #ADD java to PATH 
-    echo "export PATH=\$PATH:$root_dir/java/jdk8u345-b01-jre/bin" >> /etc/profile
-    source /etc/profile
     finishLog "Installing Java 8"
+
+    startLog "Create Wrapper Start Script For Using Java 8"
+    touch $start_file
+    echo "PATH=$root_dir/java/jdk8u345-b01-jre/bin" >> $start_file
+    echo "$root_dir/ServerStart.sh" >> $start_file
+    sudo -H -u mcserver bash -c "cd $root_dir && chmod +x $start_file"
+    finishLog "Create Wrapper Start Script For Using Java 8"
 
     startLog "Downloading ModPack"
     sudo -H -u mcserver bash -c "cd $root_dir && wget https://dist.creeper.host/FTB2/modpacks/Regrowth/1_0_2/RegrowthServer.zip -O mc-server.zip"
@@ -98,7 +103,7 @@ then
     sudo -H -u mcserver bash -c "cd $root_dir && unzip mc-server.zip"
     sudo -H -u mcserver bash -c "cd $root_dir && chmod +x ServerStart.sh"
     sudo -H -u mcserver bash -c "cd $root_dir && chmod +x Install.sh"
-    sudo -H -u mcserver bash -c "source /etc/profile && cd $root_dir && sh Install.sh"
+    sudo -H -u mcserver bash -c "PATH=$root_dir/java/jdk8u345-b01-jre/bin && cd $root_dir && sh Install.sh"
     finishLog "Installing ModPack"
 else
     generalLog "Found [$root_dir/ServerStart.sh]. "
@@ -113,10 +118,10 @@ then
     echo "[Unit]" >>/etc/systemd/system/mcserver.service
     echo "Description=MCServer" >>/etc/systemd/system/mcserver.service
     echo "[Service]" >>/etc/systemd/system/mcserver.service
-    echo "Environment=PATH=$root_dir/java/jdk8u345-b01-jre/bin" >>/etc/systemd/system/mcserver.service
+    # echo "Environment=PATH=$root_dir/java/jdk8u345-b01-jre/bin" >>/etc/systemd/system/mcserver.service
     echo "User=mcserver" >>/etc/systemd/system/mcserver.service
     echo "WorkingDirectory=$root_dir" >> /etc/systemd/system/mcserver.service
-    echo "ExecStart=\"$root_dir/ServerStart.sh\"" >>/etc/systemd/system/mcserver.service
+    echo "ExecStart=\"$start_file\"" >>/etc/systemd/system/mcserver.service
     echo "Restart=always" >>/etc/systemd/system/mcserver.service
     echo "[Install]" >>/etc/systemd/system/mcserver.service
     echo "WantedBy=multi-user.target" >>/etc/systemd/system/mcserver.service
