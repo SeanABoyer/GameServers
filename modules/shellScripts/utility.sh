9790c0c2ce2b95638417a82b0a6dd76f8475f758
Sean Boyer <seanABoyer@gmail.com>
2023-11-14 22:46:09 -0800

    Send It!

--- modules/shellScripts/utility.sh
+++ modules/shellScripts/utility.sh
@@ -9,3 +9,8 @@ finishLog () {
     date=$(date '+%d/%m/%Y %H:%M:%S')
     echo "[$date][Completed] $log_message"
 }
+generalLog () {
+    log_message=$1
+    date=$(date '+%d/%m/%Y %H:%M:%S')
+    echo "[$date][Info] $log_message"
+}
