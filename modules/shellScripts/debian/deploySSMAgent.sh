ba25cae15df848a848e8ff3cd3b8c341b0fad00c
Sean Boyer <seanABoyer@gmail.com>
2023-11-16 23:53:55 -0800

    test

--- modules/shellScripts/debian/deploySSMAgent.sh
+++ modules/shellScripts/debian/deploySSMAgent.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 dir="/tmp/ssmAgent"
-startLog "Updating System"
+bash -c "source utility.sh; startLog" "Updating System"
+#startLog "Updating System"
 mkdir -p "$dir"
 cd "$dir"
 wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
 sudo dpkg -i amazon-ssm-agent.deb
-finishLog "Updating System"
+bash -c "source utility.sh; finishLog" "Updating System"
+#finishLog "Updating System"
