4aa7c9621c9638cbc4e2e91fc6ed2937906c2732
Sean Boyer <seanABoyer@gmail.com>
2023-11-16 22:07:46 -0800

    tewst

--- modules/shellScripts/mountEFS.sh
+++ modules/shellScripts/mountEFS.sh
@@ -9,7 +9,10 @@ cd "/tmp/${filesystem_id}"
 sudo git clone https://github.com/aws/efs-utils .
 sudo ./build-deb.sh
 sudo apt install ./build/amazon-efs-utils*deb -y
+
 sudo mkdir -p $root_dir
+mkdir -p /mnt/test
+sudo mkdir -p /mnt/test2
 sudo chown -R $username:$username $root_dir
 generalLog "Attempting to mount $filesystem_id to $root_dir"
 sudo mount -t efs -o tls,iam "$filesystem_id" $root_dir
