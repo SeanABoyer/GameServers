f995dc6a990f576aca1ba7dbe16cefdbfc092d70
Sean Boyer <seanABoyer@gmail.com>
2023-11-17 00:44:48 -0800

    yes

--- modules/shellScripts/createUser.sh
+++ modules/shellScripts/createUser.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-username="${username}"
-password="${password}"
-
-echo "[$(date '+%d/%m/%Y %H:%M:%S')][Starting] Creating User:$username"
-sudo useradd $username -p $password -m
-sudo chown -R $username:$username /home/$username
-echo "[$(date '+%d/%m/%Y %H:%M:%S')][Completed] Creating User:$username"
+echo "[$(date '+%d/%m/%Y %H:%M:%S')][Starting] Creating User:${username}"
+sudo useradd ${username} -p ${password} -m
+sudo chown -R ${username}:${username} /home/${username}
+echo "[$(date '+%d/%m/%Y %H:%M:%S')][Completed] Creating User:${username}"
--- modules/shellScripts/mountEFS.sh
+++ modules/shellScripts/mountEFS.sh
@@ -1,21 +1,18 @@
-root_dir="${root_dir}"
-filesystem_id="${filesystem_id}"
-username="${username}"
 dir="/tmp/efsUtils"
 echo "[$(date '+%d/%m/%Y %H:%M:%S')][Starting] Mounting EFS"
 echo "SEAN $root_dir SEAN"
 sudo apt install git binutils -y
-mkdir -p "$dir"
-cd "$dir"
+mkdir -p "${dir}"
+cd "${dir}"
 sudo git clone https://github.com/aws/efs-utils .
 sudo ./build-deb.sh
 sudo apt install ./build/amazon-efs-utils*deb -y
 
-mkdir -p "$root_dir"
-cd "$root_dir"
+mkdir -p "${root_dir}"
+cd "${root_dir}"
 
-sudo chown -R $username:$username $root_dir
-generalLog "Attempting to mount $filesystem_id to $root_dir"
-sudo mount -t efs -o tls,iam "$filesystem_id" $root_dir
-sudo echo "$filesystem_id:/ $root_dir efs _netdev,noresvport,tls,iam 0 0" >> /etc/fstab
+sudo chown -R ${username}:${username} ${root_dir}
+generalLog "Attempting to mount ${filesystem_id} to ${root_dir}"
+sudo mount -t efs -o tls,iam "${filesystem_id}" ${root_dir}
+sudo echo "{$filesystem_id}:/ ${root_dir} efs _netdev,noresvport,tls,iam 0 0" >> /etc/fstab
 echo "[$(date '+%d/%m/%Y %H:%M:%S')][Completed] Mounting EFS"
