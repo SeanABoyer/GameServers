c67686e9ef4cb4e5e702d97cef61e0906761eb1a
SeanABoyer <SeanABoyer@gmail.com>
2023-11-24 01:45:32 -0800

    .

--- modules/shellScripts/createWrapperScripts.sh
+++ modules/shellScripts/createWrapperScripts.sh
@@ -2,8 +2,8 @@
 startFileFullPath=${root_dir}/startServer.sh
 stopFileFullPath=${root_dir}/stopServer.sh
 
-if [! -f "$startFileFullPath" ]
-then
+#if [! -f "$startFileFullPath" ]
+#then
     echo "[$(date '+%d/%m/%Y %H:%M:%S')][Starting] Creating Wrapper Start Script: ${gamename}"
     touch $startFileFullPath
     echo "bash ${$root_dir}/cs2server update >> $startFileFullPath"
@@ -11,19 +11,19 @@ then
     sudo chown GameAdmin:GameAdmin $startFileFullPath
     sudo chmod +x $startFileFullPath
     echo "[$(date '+%d/%m/%Y %H:%M:%S')][Completed] Creating Wrapper Start Script: ${gamename}"
-else
+#else
     echo "[$(date '+%d/%m/%Y %H:%M:%S')][INFO] Wrapper Start Script: $startFileFullPath exists."
-fi
+#fi
 
 
-if [! -f "$stopFileFullPath" ]
-then
+#if [! -f "$stopFileFullPath" ]
+#then
     echo "[$(date '+%d/%m/%Y %H:%M:%S')][Starting] Creating Wrapper Stop Script: ${gamename}"
     touch $stopFileFullPath
     echo "bash ${$root_dir}/cs2server stop >> $stopFileFullPath"
     sudo chown GameAdmin:GameAdmin $stopFileFullPath
     sudo chmod +x $stopFileFullPath
     echo "[$(date '+%d/%m/%Y %H:%M:%S')][Completed] Creating Wrapper Stop Script: ${gamename}"
-else
+#else
     echo "[$(date '+%d/%m/%Y %H:%M:%S')][INFO] Wrapper Stop Script: $stopFileFullPath exists."
-fi
+#fi
