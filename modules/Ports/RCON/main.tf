ed0474ce8f0d3b41e8cb83018a14a0837dde3cd5
SeanABoyer <SeanABoyer@gmail.com>
2023-11-25 08:21:54 -0800

    Port Correciton

--- modules/Ports/RCON/main.tf
+++ modules/Ports/RCON/main.tf
@@ -1,17 +1,17 @@
-resource "aws_security_group_rule" "WEB_80"{
+resource "aws_security_group_rule" "RCON_Web"{
   type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
+  from_port         = 4326
+  to_port           = 4326
+  protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = var.ec2_security_group_id
 }
 
-resource "aws_security_group_rule" "WEB_443"{
+resource "aws_security_group_rule" "RCON_WebSocket"{
   type              = "egress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
+  from_port         = 4327
+  to_port           = 4327
+  protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = var.ec2_security_group_id
 }
