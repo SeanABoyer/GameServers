ba5787190f7d3b5abe49031d874e4bbb63693c08
SeanABoyer <SeanABoyer@gmail.com>
2023-11-27 15:11:34 -0800

    update rcon to just TCP

--- modules/Ports/RCON/main.tf
+++ modules/Ports/RCON/main.tf
@@ -2,7 +2,7 @@ resource "aws_security_group_rule" "RCON_Web"{
   type              = "ingress"
   from_port         = 3000
   to_port           = 3000
-  protocol          = "-1"
+  protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = var.ec2_security_group_id
 }
