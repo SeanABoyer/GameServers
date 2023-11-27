882b804eb9d8bd04f66a52f98e54f70805b3123d
SeanABoyer <SeanABoyer@gmail.com>
2023-11-27 12:12:57 -0800

    File reorg and variable renaming for structure change.

--- plans/CS2/main.tf
+++ plans/CS2/main.tf
@@ -7,154 +7,129 @@ locals {
     gamename = "CS2"
     lgsmfilename = "cs2server"
     username = "GameAdmin"
-    rootdir = "/mnt/${local.gamename}"
-    rcondir = "${local.rootdir}/cs2-rcon-panel-master"
+    rootdir = "/mnt/${module.server.efs_file_system_id}"
+    gamebasedir = "${local.rootdir}/CS2"
+    rconbasedir = "${local.rootdir}/cs2-rcon-panel-master"
     CS2startScriptFullPath = "${local.rootdir}/startServer.sh"
     CS2stopScriptFullPath = "${local.rootdir}/stopServer.sh"
-    RCONstartScriptFullPath = "${local.rcondir}/startServer.sh"
-    RCONstopScriptFullPath = "${local.rcondir}/stopServer.sh"
+    RCONstartScriptFullPath = "${local.rconbasedir}/startServer.sh"
+    RCONstopScriptFullPath = "${local.rconbasedir}/stopServer.sh"
+    scripts = [
+        {
+            filename:"00_install_ssm_agent",
+            content:templatefile(
+                "${path.root}/modules/shellScripts/debian/ssmAgent.sh",
+                {}
+            )
+        },
+        {
+            filename:"01_update_system",
+            content:templatefile(
+                "${path.root}/modules/shellScripts/debian/update.sh",
+                {}
+            )
+        },
+        {
+            filename:"02_create_user",
+            content:templatefile(
+                "${path.root}/modules/shellScripts/utility/createUser.sh",
+                {
+                    username = "${local.username}"
+                    password = "${random_password.password.result}"
+                }
+            )
+        },
+        {
+            filename:"03_mount_efs",
+            content:templatefile(
+                "${path.root}/modules/shellScripts/utility/mountEFS.sh",
+                {
+                    username = "${local.username}"
+                    root_dir = "${local.rootdir}"
+                    filesystem_id = module.server.efs_file_system_id
+                }
+            )
+        },
+        {
+            filename:"04_install_cs2",
+            content:templatefile(
+                "CS2Install.sh",
+                {
+                    steamUsername = "${var.steamUsername}"
+                    steamPassword = "${var.steamPassword}"
+                    gslt = "${var.steamGSLT}"
+                    lgsmfilename = "${local.lgsmfilename}"
+                    game_dir = "${local.gamebasedir}"
+                    gamename = "${local.gamename}"
+                    startScriptFullPath = "${local.CS2startScriptFullPath}"
+                    stopScriptFullPath = "${local.CS2stopScriptFullPath}"
+                }
+            )
+        },
+        {
+            filename:"05_create_cs2_service",
+            content:templatefile(
+                "${path.root}/modules/shellScripts/utility/createService.sh",
+                {
+                    gamename = "${local.gamename}"
+                    username = "${local.username}"
+                    dir = "${local.gamebasedir}"
+                    startScriptFullPath = "${local.CS2startScriptFullPath}"
+                    stopScriptFullPath = "${local.CS2stopScriptFullPath}"
+                }
+            )
+        },
+        {
+            filename:"06_start_cs2_service",
+            content:templatefile(
+                "${path.root}/modules/shellScripts/utility/startService.sh",
+                {
+                    gamename = "${local.gamename}"
+                }
+            )
+        },
+        {
+            filename:"07_install_rcon_web_app",
+            content:templatefile(
+                "${path.root}/modules/shellScripts/rcon/installRcon.sh",
+                {
+                    username = "${local.username}"
+                    gamename = "${local.gamename}_RCON"
+                    rcon_dir = "${local.rconbasedir}"
+                    startScriptFullPath = "${local.RCONstartScriptFullPath}"
+                    stopScriptFullPath = "${local.RCONstopScriptFullPath}"
+                }
+            )
+        },
+        {
+            filename:"08_create_rcon_service",
+            content:templatefile(
+                "${path.root}/modules/shellScripts/utility/createService.sh",
+                {
+                    gamename = "${local.gamename}_RCON"
+                    username = "${local.username}"
+                    root_dir = "${local.rconbasedir}"
+                    startScriptFullPath = "${local.RCONstartScriptFullPath}"
+                    stopScriptFullPath = "${local.RCONstopScriptFullPath}"
+                }
+            )
+        },
+        {
+            filename:"09_start_rcon_service",
+            content:templatefile(
+                "${path.root}/modules/shellScripts/utility/startService.sh",
+                {
+                    gamename = "${local.gamename}_RCON"
+                }
+            )
+        }
+    ]
 }
-
-data "template_file" "CS2Install" {
-    template = "${file("./CS2Install.sh")}"
-    vars = {
-        steamUsername = "${var.steamUsername}"
-        steamPassword = "${var.steamPassword}"
-        gslt = "${var.steamGSLT}"
-        lgsmfilename = "${local.lgsmfilename}"
-        root_dir = "${local.rootdir}"
-        gamename = "${local.gamename}"
-        startScriptFullPath = "${local.CS2startScriptFullPath}"
-        stopScriptFullPath = "${local.CS2stopScriptFullPath}"
-    }
-}
-
-data "template_file" "createCS2Service" {
-    template = "${file("../../modules/shellScripts/createService.sh")}"
-    vars = {
-        gamename = "${local.gamename}"
-        username = "${local.username}"
-        root_dir = "${local.rootdir}"
-        startScriptFullPath = "${local.CS2startScriptFullPath}"
-        stopScriptFullPath = "${local.CS2stopScriptFullPath}"
-
-    }
-}
-
-data "template_file" "installRcon" {
-    template = "${file("../../modules/shellScripts/installRcon.sh")}"
-    vars = {
-        username = "${local.username}"
-        gamename = "${local.gamename}_RCON"
-        rcon_dir = "${local.rcondir}"
-        startScriptFullPath = "${local.RCONstartScriptFullPath}"
-        stopScriptFullPath = "${local.RCONstopScriptFullPath}"
-    }
-}
-
-data "template_file" "createRconService" {
-    template = "${file("../../modules/shellScripts/createService.sh")}"
-    vars = {
-        gamename = "${local.gamename}_RCON"
-        username = "${local.username}"
-        root_dir = "${local.rcondir}"
-        startScriptFullPath = "${local.RCONstartScriptFullPath}"
-        stopScriptFullPath = "${local.RCONstopScriptFullPath}"
-
-    }
-}
-
-data "template_file" "startCS2Service" {
-    template = "${file("../../modules/shellScripts/startService.sh")}"
-    vars = {
-        gamename = "${local.gamename}"
-    }
-}
-
-data "template_file" "startRCONService" {
-    template = "${file("../../modules/shellScripts/startService.sh")}"
-    vars = {
-        gamename = "${local.gamename}_RCON"
-    }
-}
-
-
-
-
-data "template_file" "createUser" {
-    template = "${file("../../modules/shellScripts/createUser.sh")}"
-    vars = {
-        username = "${local.username}"
-        password = "${random_password.password.result}"
-    }
-}
-
-data "template_file" "mountEFS" {
-    template = "${file("../../modules/shellScripts/mountEFS.sh")}"
-    vars = {
-        username = "${local.username}"
-        root_dir = "${local.rootdir}"
-        filesystem_id = module.server.efs_file_system_id
-    }
-}
-
-data "template_file" "update" {
-    template = "${file("../../modules/shellScripts/debian/update.sh")}"
-    vars = {}
-}
-
-data "template_file" "debianSSMAgent" {
-    template = "${file("../../modules/shellScripts/debian/deploySSMAgent.sh")}"
-    vars = {}
-}
-
-
 
 
 module "server"{
     source = "../../modules/Server"
-    scripts = [
-        {
-            "filename":"01_debianSSMAgent.sh",
-            "content":data.template_file.debianSSMAgent.rendered
-        },
-        {
-            "filename":"02_update.sh",
-            "content":data.template_file.update.rendered
-        },
-        {
-            "filename":"03_createUser.sh",
-            "content":data.template_file.createUser.rendered
-        },
-        {
-            "filename":"04_mountEFS.sh",
-            "content":data.template_file.mountEFS.rendered
-        },
-        {
-            "filename":"05_CS2Install.sh",
-            "content":data.template_file.CS2Install.rendered
-        },
-        {
-            "filename":"06_createCS2Service.sh",
-            "content":data.template_file.createCS2Service.rendered
-        },
-        {
-            "filename":"07_startCS2Service.sh",
-            "content":data.template_file.startCS2Service.rendered
-        },
-        {
-            "filename":"08_installRcon.sh",
-            "content":data.template_file.installRcon.rendered
-        },
-        {
-            "filename":"08_createRCONService.sh",
-            "content":data.template_file.createRconService.rendered
-        },
-        {
-            "filename":"08_startRCONService.sh",
-            "content":data.template_file.startRCONService.rendered
-        }
-    ]
+    scripts = local.scripts
     game_name = local.gamename
     instance_type = "t3.small"
     ami_name = "debian-11-amd64*"
