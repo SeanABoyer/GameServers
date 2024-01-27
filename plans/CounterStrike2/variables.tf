3b13161889a0d8a48e6a4a46f3a3ffeeea3e2be4
SeanABoyer <SeanABoyer@gmail.com>
2024-01-27 13:56:20 -0800

    Removing type around contstraints

--- plans/CounterStrike2/variables.tf
+++ plans/CounterStrike2/variables.tf
@@ -1,9 +1,9 @@
 variable "cidr_block" {
-    type = "string"
+    type = string
     default = "10.0.0.0/16"
 }
 
 variable "game_name"{
-    type = "string"
+    type = string
     default = "Counter-Strike-2"
 }
