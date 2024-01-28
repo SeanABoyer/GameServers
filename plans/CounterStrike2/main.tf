61c1fbc0e0dd58685b876a2bff993199d132b183
SeanABoyer <SeanABoyer@gmail.com>
2024-01-27 18:48:30 -0800

    test

--- plans/CounterStrike2/main.tf
+++ plans/CounterStrike2/main.tf
@@ -1,12 +1,12 @@
 terraform {
-    required_version = ">=1.1.3"
     required_providers {
       aws = {
         source  = "hashicorp/aws"
-        version = "4.40.0"
+        version = "5.34.0"
       }
     }
 }
+
 provider "aws" {
     default_tags {
         tags = {
@@ -27,7 +27,6 @@ data "aws_availability_zones" "availability_zones" {
 resource "aws_internet_gateway" "gw" {
     vpc_id = aws_vpc.vpc.id
 }
-
 
 resource "aws_route" "route_ign_to_vpc" {
   route_table_id = aws_vpc.vpc.main_route_table_id
@@ -54,56 +53,6 @@ resource "aws_subnet" "subnet_az_two" {
   cidr_block = cidrsubnet(var.cidr_block,8,1)
   availability_zone = data.aws_availability_zones.availability_zones.names[1]
 }
-
-# resource "aws_security_group" "alb_sg"{
-#     name =  "${var.game_name}-alb-sg"
-#     vpc_id = aws_vpc.vpc.id
-#     ingress {
-#         from_port = 27015
-#         to_port = 27015
-#         protocol = "tcp"
-#         cidr_blocks = ["0.0.0.0/0"]
-#     }
-
-#     ingress {
-#         from_port = 27015
-#         to_port = 27015
-#         protocol = "udp"
-#         cidr_blocks = ["0.0.0.0/0"]
-#     }
-
-#     egress {
-#         from_port   = 0
-#         to_port     = 0
-#         protocol    = "-1"
-#         cidr_blocks = ["0.0.0.0/0"]
-#     }
-# }
-
-# resource "aws_lb" "alb" {
-#   name = "${var.game_name}-alb"
-#   load_balancer_type = "application"
-#   subnets = [aws_subnet.subnet_az_one.id,aws_subnet.subnet_az_two.id]
-#   security_groups = [aws_security_group.alb_sg.id] 
-# }
-
-# resource "aws_lb_target_group" "alb_target_group" {
-#     name = "${var.game_name}-alb-tg"
-#     port = 27015
-#     protocol = "TCP_UDP"
-#     target_type = "ip"
-#     vpc_id = aws_vpc.vpc.id
-# }
-
-# resource "aws_lb_listener" "alb_listener" {
-#   load_balancer_arn = aws_lb.alb.arn
-#   port = 27015
-#   protocol = "TCP_UDP"
-#   default_action {
-#     type = "forward"
-#     target_group_arn = aws_lb_target_group.alb_target_group.arn
-#   }
-# }
 #END NETWORKING
 
 resource "aws_ecs_cluster" "cluster" {
@@ -169,7 +118,7 @@ resource "aws_ecs_task_definition" "task" {
 resource "aws_efs_file_system" "efs" {}
 resource "aws_efs_mount_target" "efs_mount_target" {
   file_system_id = aws_efs_file_system.efs.id
-  subnet_id      = aws_subnet.subnet_az_one
+  subnet_id      = aws_subnet.subnet_az_one.id
   security_groups = [aws_security_group.efs_sg.id]
 }
 resource "aws_security_group" "efs_sg" {
@@ -216,12 +165,6 @@ resource "aws_ecs_service" "ecs_service" {
   task_definition = aws_ecs_task_definition.task.arn
   launch_type = "FARGATE"
   desired_count = 1
-
-#   load_balancer {
-#     target_group_arn = aws_lb_target_group.alb_target_group.arn
-#     container_name = aws_ecs_task_definition.task.family
-#     container_port = 27015
-#   }
 
   network_configuration {
     subnets = [aws_subnet.subnet_az_one.id,aws_subnet.subnet_az_two.id]
