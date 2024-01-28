e77a2b7d6b109326397e42183f66d41d98d9f4d2
SeanABoyer <SeanABoyer@gmail.com>
2024-01-27 17:36:44 -0800

    name/tags

--- plans/CounterStrike2/main.tf
+++ plans/CounterStrike2/main.tf
@@ -7,7 +7,7 @@ data "aws_availability_zones" "availability_zones" {
 
 #START NETWORKING
 resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.vpc.id
+    vpc_id = aws_vpc.vpc.id
 }
 
 
@@ -18,7 +18,11 @@ resource "aws_route" "route_ign_to_vpc" {
 }
 
 resource "aws_vpc" "vpc" {
-  cidr_block = var.cidr_block
+    
+    cidr_block = var.cidr_block
+    tags = {
+      name =  "${var.game_name}-vpc"
+    }
 }
 
 resource "aws_subnet" "subnet_az_one" {
@@ -33,29 +37,30 @@ resource "aws_subnet" "subnet_az_two" {
   availability_zone = data.aws_availability_zones.availability_zones.names[1]
 }
 
-# resource "aws_security_group" "alb_sg"{
-#     vpc_id = aws_vpc.vpc.id
-#     ingress {
-#         from_port = 27015
-#         to_port = 27015
-#         protocol = "tcp"
-#         cidr_blocks = ["0.0.0.0/0"]
-#     }
+resource "aws_security_group" "alb_sg"{
+    name =  "${var.game_name}-alb-sg"
+    vpc_id = aws_vpc.vpc.id
+    ingress {
+        from_port = 27015
+        to_port = 27015
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-#     ingress {
-#         from_port = 27015
-#         to_port = 27015
-#         protocol = "udp"
-#         cidr_blocks = ["0.0.0.0/0"]
-#     }
+    ingress {
+        from_port = 27015
+        to_port = 27015
+        protocol = "udp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-#     egress {
-#         from_port   = 0
-#         to_port     = 0
-#         protocol    = "-1"
-#         cidr_blocks = ["0.0.0.0/0"]
-#     }
-# }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
 
 # resource "aws_lb" "alb" {
 #   name = "${var.game_name}-alb"
